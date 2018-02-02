b4fc7b8f41f773f7f3896fa88dbdc0a737d64b7d
FreeBird <1032796097@qq.com>
2018-02-02 15:05:20 +0800

    修改了实验管理的样式，预约列表改为从easymock模拟借口来获取数据

--- src/app/routes/yygl/yygl.component.ts
+++ src/app/routes/yygl/yygl.component.ts
@@ -4,7 +4,8 @@ import {Component, OnInit} from '@angular/core';
 import {FormGroup, Validators, FormBuilder} from '@angular/forms';
 import {YyglService} from './yygl.service';
 import {Router} from '@angular/router';
-import {NzMessageService} from 'ng-zorro-antd';
+import {NzMessageService, NzModalService} from 'ng-zorro-antd';
+import {UserService} from '../user/user.service';
 
 @Component({
   selector: 'Yygl',
@@ -13,33 +14,41 @@ import {NzMessageService} from 'ng-zorro-antd';
   providers: [YyglService]
 })
 
-export class YyglComponent {
+export class YyglComponent implements OnInit {
     _value = ''; /*搜索内容*/
-    choice = 'all'; /*筛选条件:全部：all 进行中：ing 未开始：no*/
+    choice = 100; /*筛选条件:全部：100 进行中：0 未开始：0*/
     /*预约情况：预约课程数，已预约学时，待通过预约数*/
     orders = { orderednum : 3 , orderedhour: 64, ordering: 3};
     /*预约列表*/
-    data = [
+    constructor(private yyglService: YyglService, private message: NzMessageService, private modalService: NzModalService) {
+    }
+    data = [];
+    /*data = [
         {
-            course    : '数据结构课程设计', /*课程名称*/
-            coursetime : '周一123节 1-17周', /*课程时间*/
-            orderedhour : 16, /*已预约学时*/
-            ordertime:  '2016-06-16 14:03', /*预约提交时间*/
-            progress : '60',
-        }, {
-            course    : '数据结构课程设计',
-            coursetime : '周一123节 1-17周',
-            orderedhour : 16,
-            ordertime:  '2016-06-16 14:03',
-            progress : '60',
-        }, {
-            course    : '数据结构课程设计',
-            coursetime : '周一123节 1-17周',
-            orderedhour : 16,
-            ordertime:  '2016-06-16 14:03',
-            progress : '60',
+            id: 1,
+            course    : '', /!*课程名称*!/
+            coursetime : '', /!*课程时间*!/
+            orderedhour : 16, /!*已预约学时*!/
+            ordertime:  '', /!*预约提交时间*!/
+            progress : '',
         }
-    ];
+    ];*/
+    private _getData = () => {
+        this.yyglService.getOrderList()
+            .then((result: any) => {
+                const { data } = result;
+                this.data = data;
+            });
+        this.yyglService.getOrder()
+            .then((result: any) => {
+                const {data} = result;
+                this.orders = data;
+            });
+    }
+
+    ngOnInit(): void {
+        this._getData();
+    }
     onSearch(event: string): void {
         console.log(event);
     }
--- src/app/routes/yygl/yygl.service.ts
+++ src/app/routes/yygl/yygl.service.ts
@@ -1,9 +1,31 @@
 import {Injectable} from '@angular/core';
 import {SessionStorageService} from '@core/storage/storage.module';
+import {NHttpClinet} from '@core/utils/http.client';
+import {BaseService} from '@core/utils/BaseRequest';
+import {reject} from 'q';
 
 @Injectable()
-export class YyglService {
-  constructor(private _storage: SessionStorageService) {
+export class YyglService extends BaseService {
+  constructor(private _storage: SessionStorageService, http: NHttpClinet) {
+    super('yygl', http);
   }
 
+  getOrder() {
+    const yyqk = 'https://www.easy-mock.com/mock/5a73c90cb4ec7020fa2f63e8/example/yyqk';
+      return new Promise((resolve, reject) => {
+          this.http.get(yyqk)
+              .subscribe(result => {
+                  resolve(result);
+              });
+      });
+  }
+
+    getOrderList() {
+        return new Promise((resolve, reject) => {
+            this.http.get('https://www.easy-mock.com/mock/5a73c90cb4ec7020fa2f63e8/yygl')
+                .subscribe(result => {
+                    resolve(result);
+                });
+        });
+    }
 }
