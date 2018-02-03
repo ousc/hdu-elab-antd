ee87537b104cb9cc0473a6f77590a1696d98f1ad
ousc <750154432@qq.com>
2018-02-03 11:48:45 +0800

    order前三步完成

--- src/app/routes/order/order.component.ts
+++ src/app/routes/order/order.component.ts
@@ -5,6 +5,7 @@ import {OrderService} from './order.service';
 import {Router} from '@angular/router';
 import {NzMessageService, NzModalService} from 'ng-zorro-antd';
 import {SessionStorageService} from "@core/storage/storage.service";
+import {isUndefined} from "util";
 
 @Component({
     selector: 'app-order',
@@ -18,15 +19,9 @@ export class OrderComponent implements OnInit {
     }
     validateForm: FormGroup;//定义表单验证
     loadStatus: boolean;//加载状况
-    labdata = [{"checked":false,PeoCount:'0',zhiyuan:1,status:false,"id":2,"adminId":2,"adminName":"张三","labName":"软件开发综合实验室","labType":"软件实验室","laPeoCount":100,"labBuild":1,"labNumber":105,"labIntroduce":"a"},
-        {"checked":false,PeoCount:'0',status:false,zhiyuan:1,"id":3,"adminId":3,"adminName":"李四","labName":"安卓开发实验室","labType":"软件实验室","labPeoCount":100,"labBuild":3,"labNumber":317,"labIntroduce":"a"},
-        {"checked":false,PeoCount:'0',status:false,zhiyuan:2,"id":3,"adminId":3,"adminName":"李四","labName":"安卓开发实验室","labType":"软件实验室","labPeoCount":100,"labBuild":3,"labNumber":317,"labIntroduce":"a"},
-        {"checked":false,PeoCount:'0',status:false,zhiyuan:2,"id":3,"adminId":3,"adminName":"李四","labName":"安卓开发实验室","labType":"软件实验室","labPeoCount":100,"labBuild":3,"labNumber":317,"labIntroduce":"a"},
-        {"checked":false,PeoCount:'0',status:false,zhiyuan:3,"id":3,"adminId":3,"adminName":"李四","labName":"安卓开发实验室","labType":"软件实验室","labPeoCount":100,"labBuild":3,"labNumber":317,"labIntroduce":"a"},
-        {"checked":false,PeoCount:'0',status:false,zhiyuan:3,"id":3,"adminId":3,"adminName":"李四","labName":"安卓开发实验室","labType":"软件实验室","labPeoCount":100,"labBuild":3,"labNumber":317,"labIntroduce":"a"},
-        {"checked":false,PeoCount:'0',status:true, zhiyuan:3,"id":4,"adminId":4,"adminName":"王五","labName":"苹果开发实验室","labType":"软件实验室","labPeoCount":100,"labBuild":3,"labNumber":318,"labIntroduce":null}
-    ];
+    labdata = [[],[],[]];
     //获取的实验室信息
+    zhiyuandata = [];
     submitBtn = '下一步';
     submitBackBtn = '上一步';
     current = 0;//初始化步骤
@@ -37,7 +32,6 @@ export class OrderComponent implements OnInit {
         { value: '101123124', label: '数据结构课程设计{周一456节 1-17周}',className:'数据结构课程设计',teacherId:'12332123',classPeoCount:120 },
         { value: '101123125', label: '数据结构课程设计{周一678节 1-17周}',className:'数据结构课程设计',teacherId:'12332123',classPeoCount:120 }
     ];//课程信息
-
     week = [{ value:1, label: '1' },
         { value:2, label: '2' },
         { value:3, label: '3' },
@@ -105,7 +99,8 @@ export class OrderComponent implements OnInit {
         thirdLaPeoCount:null
     };
     submit(n): void {
-        let url = ['接口地址1', '接口地址2'];//改为接口地址
+        let url = ['https://www.easy-mock.com/mock/5a5b5ba932a4fc7429df228a/lab1',
+            '接口地址2'];//改为接口地址
         switch (n) {
             case 0: {
                 for (let i = 1; i < 4; i++) {
@@ -128,35 +123,41 @@ export class OrderComponent implements OnInit {
                         classNum: classNumtemp,//第几节
                         type: typetemp//种类
                     };
-                    this.lastData.classId = data.course[0].classId;
-                    this.lastData.className = data.course[0].className;
-                    this.lastData.classpeoCount = data.course[0].classpeoCount;
-                    this.lastData.teacherId = data.course[0].teacherId;
-                    // let res = this.orderService.executeHttp(url[n],{username:this._storage.get('username'),data:data,no:i});
-                    // if(res['result']!='success'){
-                    //   this.info('志愿'+i+'提交失败,请检查网络连接后重试！');
-                    //   return;
-                    // }else{
-                    //   for(let i=0;i<res['lab'].length;i++){
-                    //    res['lab']['checked']=false;
-                    //    res['lab']['PeoCount']=0;
-                    //    res['zhiyuan'] = i+1;
-                    //    this.labdata.push(res['lab'][i]);
-                    //   }
-                    // }
-                    console.log('已将{username:'+this._storage.get('username')+',data:'+JSON.stringify(data)+",zhiyuan"+i+1+'}提交到' + url[0]);
+                    console.log(this.validateForm.controls['course'].value);
+                    this.lastData.classId = this.validateForm.controls['course'].value.value;
+                    this.lastData.className = this.validateForm.controls['course'].value.className;
+                    this.lastData.classpeoCount = this.validateForm.controls['course'].value.classPeoCount;
+                    this.lastData.teacherId = this.validateForm.controls['course'].value.teacherId;
+                    console.log(this.lastData);
+                    this.orderService.executeHttp(url[n],{username:this._storage.get('username'),data:data,no:i}).then((result: any) => {
+                        let res = JSON.parse(result['_body']);
+                        console.log(res);
+                        if(res['result']!='success'){
+                            this.info('志愿'+i+'提交失败,请检查网络连接后重试！');
+                            return;
+                        }else{
+                            for(let k=0;k<res['lab'].length;k++){
+                                res['lab']['checked']=false;
+                                res['lab']['PeoCount']=0;
+                                res['zhiyuan'] = k+1;
+                                this.labdata[i-1].push(res['lab'][k]);
+                            }
+                        }
+                        console.log('已将{username:'+this._storage.get('username')+',data:'+JSON.stringify(data)+",zhiyuan"+i+1+'}提交到' + url[0]);
+                    });
                 }
                 this.current += 1;
                 this.submitBtn = '下一步';
                 break;
             }//第一步提交完成
             case 1: {
-                let data = [];
+                this.zhiyuandata=[];
                 for (let i = 0; i < this.labdata.length; i++)
-                    if (this.labdata[i].checked) {
-                        data.push(i);
+                    for(let j=0; j < this.labdata[i].length;j++)
+                    if (this.labdata[i][j].checked) {
+                        this.zhiyuandata.push({i:i,j:j});
                     }
-                if (data.length == 0) {
+                if (this.zhiyuandata.length == 0) {
                     this.info('请至少选择一个实验室！');
                     this.submitBtn = '下一步';
                     break;
@@ -166,18 +167,22 @@ export class OrderComponent implements OnInit {
                 break;
             }//第二步提交完成
             case 2:{
-                let data = [];
+                let peocountsum = 0;
                 for (let i = 0; i < this.labdata.length; i++)
-                    if (this.labdata[i].checked) {
-                        data.push({id:this.labdata[i].id,PeoCount:this.labdata[i].PeoCount});
-                    }
-                console.log(data);
-                // let res = this.orderService.executeHttp(url[1],{username:this._storage.get('username'),data:data});
-                // if(res.result==''success) this.current += 1;
+                    for(let j=0; j < this.labdata[i].length;j++)
+                        if (this.labdata[i][j].checked) {
+                            peocountsum+=this.labdata[i][j].PeoCount;
+                        }
+                if(peocountsum!=this.lastData.classpeoCount){
+                    console.log(peocountsum);
+                    console.log(this.lastData.classpeoCount)
+                    this.info('预约实验室人数必须等于班级总人数！');
+                    break;
+                }
                 this.current += 1;
                 this.submitBtn = '下一步';
                 break;
-            }
+            }//第三步提交完成
         }
     }
 
@@ -217,6 +222,9 @@ export class OrderComponent implements OnInit {
         }
     }
     back(){
+        if(this.current==1){
+            this.labdata = [[],[],[]];
+        }
         this.loadStatus = true;
         this.current-=1;
         this.loadStatus = false;
@@ -250,7 +258,6 @@ export class OrderComponent implements OnInit {
             this.validateForm.setValue(c);
         }
     };
-    //控制全选单双重置
     getFormControl(name) {
         return this.validateForm.controls[name];
     }
