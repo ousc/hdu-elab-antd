5cb54f061f46e46f80f8a84558bdb83d22d8c23b
ousc <750154432@qq.com>
2018-02-22 23:58:53 +0800

    个人信息界面

--- src/app/routes/profile/profile.component.ts
+++ src/app/routes/profile/profile.component.ts
@@ -5,6 +5,7 @@ import {ProfileService} from './profile.service';
 import {Router} from '@angular/router';
 import {NzMessageService} from 'ng-zorro-antd';
 import {SessionStorageService} from "@core/storage/storage.service";
+import {FormBuilder, FormGroup, Validators} from "@angular/forms";
 
 
 @Component({
@@ -14,15 +15,67 @@ import {SessionStorageService} from "@core/storage/storage.service";
   providers: [ProfileService]
 })
 export class ProfileComponent implements OnInit {
-    constructor(private _storage : SessionStorageService, private ProfileService: ProfileService, private _message: NzMessageService) {
+    constructor(private _storage : SessionStorageService,private fb: FormBuilder, private ProfileService: ProfileService, private _message: NzMessageService) {
     }
+    nickname:string;
+    email:string;
+    phone:string;
+    editMode=false;
+    editText='编辑';
     getUserInfo(){
         this.ProfileService.executeHttp("user/getUserByUserName",{"userName":this._storage.get("username")}).then((result: any) => {
-            let res = JSON.parse(result['_body']);
-            console.log(res);
+            let res = JSON.parse(result['_body'])["User1"];
+            this.nickname = res['userNickname'];
+            this.email = res['email'];
+            this.phone = res['phone'];
         })
     }
+    edit(){
+        if(!this.editMode){
+            this.editText="返回";
+        }else{
+            this.editText="编辑";
+        }
+        this.editMode=!this.editMode;
+    }
+    cancle(){
+        this.editMode=false;
+    }
+    validateForm: FormGroup;
+
+    _submitForm() {
+        for (const i in this.validateForm.controls) {
+            this.validateForm.controls[i].markAsDirty();
+        }
+        for (const i in this.validateForm.controls) {
+            if (!this.validateForm.controls[i].valid) {
+                return;
+            }
+        }
+        this.ProfileService.executeHttp('user/updateUser', {
+            "userName":this._storage.get("username"),
+            "userNickname": this.validateForm.controls['newNickName'].value,
+            "email": this.validateForm.controls['newEmail'].value,
+            "phone": this.validateForm.controls['newPhone'].value
+        }).then((result: any) => {
+            let res = JSON.parse(result['_body'])['result'];
+            if(res=="success"){
+                this._message.success('修改成功！');
+                setTimeout(function () {
+                    window.location.assign('/');
+                },2000)
+            }else{
+                this._message.error(res);
+            }
+        })
+    }
+
     ngOnInit() {
         this.getUserInfo();
+        this.validateForm = this.fb.group({
+            newPhone: [ this.phone, [ Validators.required ] ],
+            newEmail: [ this.email, [ Validators.required ] ],
+            newNickName: [ this.nickname, [ Validators.required ] ]
+        });
     }
 }
