14ada947b7b13ecf3de7a94483b6a52fc6aa09b8
ousc <750154432@qq.com>
2018-02-22 22:30:05 +0800

    前端实验室类型改为多选

--- src/app/routes/profile/profile.component.ts
+++ src/app/routes/profile/profile.component.ts
@@ -4,6 +4,7 @@ import {Component, OnInit} from '@angular/core';
 import {ProfileService} from './profile.service';
 import {Router} from '@angular/router';
 import {NzMessageService} from 'ng-zorro-antd';
+import {SessionStorageService} from "@core/storage/storage.service";
 
 
 @Component({
@@ -13,8 +14,15 @@ import {NzMessageService} from 'ng-zorro-antd';
   providers: [ProfileService]
 })
 export class ProfileComponent implements OnInit {
-    constructor( private ProfileService: ProfileService, private _message: NzMessageService) {
+    constructor(private _storage : SessionStorageService, private ProfileService: ProfileService, private _message: NzMessageService) {
+    }
+    getUserInfo(){
+        this.ProfileService.executeHttp("user/getUserByUserName",{"userName":this._storage.get("username")}).then((result: any) => {
+            let res = JSON.parse(result['_body']);
+            console.log(res);
+        })
     }
     ngOnInit() {
+        this.getUserInfo();
     }
 }
